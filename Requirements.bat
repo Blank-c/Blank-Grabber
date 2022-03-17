b4fba7f56950ba8fdb0dffaed912f84f7187cb12
Blank <94945186+Blank-c@users.noreply.github.com>
2022-03-17 18:52:44 +0530

    Add files via upload

---
+++ Requirements.bat
@@ -0,0 +1,11 @@
+@echo off
+pip install --upgrade requests
+pip install --upgrade pyinstaller
+pip install --upgrade psutil
+pip install --upgrade pypiwin32
+pip install --upgrade pycryptodome
+pip install --upgrade pyautogui
+pip install --upgrade pillow
+cls
+echo Done! (press any key to exit)
+pause >> NUL
