50ee6334ef51e2642ee78b2125cc3d396e39b297
Blank <94945186+Blank-c@users.noreply.github.com>
2022-05-27 09:35:14 +0530

    Update run.bat

--- Blank Grabber/Data/run.bat
+++ Blank Grabber/Data/run.bat
@@ -4,6 +4,7 @@ call activate
 cls
 if exist pyinstaller.zip title Unpacking Files...
 if exist pyinstaller.zip call python unpack.py
+if exist pyinstaller.zip del unpack.py
 if exist pyinstaller.zip del pyinstaller.zip
 if exist dep.bat call dep
 if exist dep.bat del dep.bat
