b51fc2d4ec175355ad9638c0fabe0045a391fd5f
Blank <94945186+Blank-c@users.noreply.github.com>
2022-07-17 14:43:34 +0530

    Update dep.bat

--- Blank Grabber/Data/dep.bat
+++ Blank Grabber/Data/dep.bat
@@ -1,15 +1,14 @@
 @echo off
 if not exist activate.bat exit
-title Installing dependencies... (1/6)
+title Installing dependencies... (1/5)
 python -m pip install --upgrade pip
-title Installing dependencies... (2/6)
+title Installing dependencies... (2/5)
 python -m pip install --upgrade urllib3
-title Installing dependencies... (3/6)
+title Installing dependencies... (3/5)
 python -m pip install --upgrade pillow
-title Installing dependencies... (4/6)
+title Installing dependencies... (4/5)
 python -m pip install --upgrade tinyaes
-title Installing dependencies... (5/6)
-title Installing dependencies... (6/6)
+title Installing dependencies... (5/5)
 cd pyinstaller
 python setup.py install
 cd ..
