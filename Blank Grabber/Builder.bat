bedcc4ac204a02f030ae776d59709fa072f828c2
Blank <94945186+Blank-c@users.noreply.github.com>
2023-05-28 10:41:22 +0530

    Updated Python installation check

--- Blank Grabber/Builder.bat
+++ Blank Grabber/Builder.bat
@@ -1,7 +1,8 @@
 @echo off
 cd /d %~dp0
 
-where python > nul 2>&1
+title Checking Python installation...
+python --version > nul 2>&1
 if %errorlevel% neq 0 (
     echo Python is not installed! (Go to https://www.python.org/downloads and install the latest version.^)
     goto ERROR
